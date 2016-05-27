989316b647ffa92c711fda8a73b68aa9714af019
Austin Kibler <austin@austins-macbook-pro.local>
2016-05-27 13:59:02 -0500

    Created device object.

---
+++ Quick Scan/Device.swift
@@ -0,0 +1,40 @@
+//
+//  Device.swift
+//  Quick Scan
+//
+//  Created by Austin Kibler on 5/27/16.
+//  Copyright © 2016 FMOLHS. All rights reserved.
+//
+
+import UIKit
+
+class Device: NSObject {
+    
+    // MARK: Properties
+    
+    var assetTag: String
+    var serialNum: String
+    var type: String
+    
+    // ======================================================
+    
+    
+    // MARK: Initialization
+    
+    init?(assetTag: String, serialNum: String, type: String){
+        
+        // Initialize stored properties
+        self.assetTag = assetTag
+        self.serialNum = serialNum
+        self.type = type
+        
+        super.init()
+        
+        if assetTag.isEmpty || serialNum.isEmpty || type.isEmpty{
+            return nil
+        }
+    }
+    
+    // ======================================================
+
+}
