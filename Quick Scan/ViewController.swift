0d57a326a36c4621f7d6d878a6616f08c9c78464
Austin Kibler <austin@austins-macbook-pro.local>
2016-05-27 14:21:41 -0500

    Added some buttons to the scan page. Cancel button will return user to table view.

--- Quick Scan/ViewController.swift
+++ Quick Scan/ViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var assetField: UITextField!
+    @IBOutlet weak var serialField: UITextField!
+    @IBOutlet weak var typeField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +26,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // MARK: Navigation
+    
+    @IBAction func cancel(sender: UIBarButtonItem) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
 }
 
