b1e25343440ccbb508187329bc326a7520012824
Austin Kibler <austin@austins-macbook-pro.local>
2016-05-30 15:02:45 -0500

    Added a Device Info page.

---
+++ Quick Scan/DeviceInfoUIView.swift
@@ -0,0 +1,21 @@
+//
+//  DeviceInfoUIView.swift
+//  Quick Scan
+//
+//  Created by Austin Kibler on 5/30/16.
+//  Copyright © 2016 FMOLHS. All rights reserved.
+//
+
+import UIKit
+
+class DeviceInfoUIView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
