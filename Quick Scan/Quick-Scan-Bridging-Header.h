bff866823833d396be8d5795f0df796d54c308ec
Austin Kibler <austin@austins-macbook-pro.local>
2016-06-01 14:26:29 -0500

    Implemented the Captuvo SDK.

---
+++ Quick Scan/Quick-Scan-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Quick-Scan-Bridging-Header.h
+//  Quick Scan
+//
+//  Created by Austin Kibler on 6/1/16.
+//  Copyright © 2016 FMOLHS. All rights reserved.
+//
+
+#ifndef Quick_Scan_Bridging_Header_h
+#define Quick_Scan_Bridging_Header_h
+#import "Captuvo.h"
+
+
+#endif /* Quick_Scan_Bridging_Header_h */
